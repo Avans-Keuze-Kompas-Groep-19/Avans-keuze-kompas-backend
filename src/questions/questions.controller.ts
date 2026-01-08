1ee39712da934e897190f51ab6df7d258d507c36
jarivankaam <jari@jarivankaam.nl>
2026-01-08 14:22:31 +0100

    add log

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -6,7 +6,10 @@ import {
   Body,
   Put,
   Delete,
+  Req,
 } from '@nestjs/common';
+import type { Request } from 'express';
+
 import { MongoIdPipe } from '../common/pipes/mongo-id.pipe';
 import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.gaurd';
@@ -16,15 +19,19 @@ import { get } from 'axios';
 
 @Controller('questions')
 export class QuestionsController {
-    constructor(private readonly service: QuestionsService) {}
-    @Get()
-    findAll() {
-        return this.service.findAll();
-    }
+  constructor(private readonly service: QuestionsService) {}
+  @Get()
+  findAll(@Req() req: Request) {
+    console.log('[QuestionsController] GET /questions hit', {
+      host: req.headers.host,
+      url: req.originalUrl,
+    });
+    return this.service.findAll();
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Get(':id')
-    findOne(@Param('id', MongoIdPipe) id: string) {
-        return this.service.findOne(id);
-    }
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  findOne(@Param('id', MongoIdPipe) id: string) {
+    return this.service.findOne(id);
+  }
 }
