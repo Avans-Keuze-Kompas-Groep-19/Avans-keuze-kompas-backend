37bc30cb3d1123c98bd741df88cfed1341ee6d61
jarivankaam <jari@jarivankaam.nl>
2026-01-08 14:15:35 +0100

    simplify schema

--- src/questions/schemas/questions.schema.ts
+++ src/questions/schemas/questions.schema.ts
@@ -1,27 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, Schema as MongooseSchema } from 'mongoose';
-
-@Schema({ _id: false })
-export class QuizAnswer {
-    @Prop()
-    answerId: number;
-
-    @Prop()
-    text: string;
-}
-const QuizAnswerSchema = SchemaFactory.createForClass(QuizAnswer);
+import { Document } from 'mongoose';
 
 @Schema()
 export class Question extends Document {
   @Prop()
   question: string;
-
-  @Prop({ type: [QuizAnswerSchema] })
-    quiz_answers: QuizAnswer[];
 }
 
 export const QuestionSchema = SchemaFactory.createForClass(Question);
-
-(QuestionSchema as MongooseSchema).add({
-    _id: Number,
-});
