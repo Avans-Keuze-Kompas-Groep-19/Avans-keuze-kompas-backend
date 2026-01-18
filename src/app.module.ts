b6e6eb1b46a648c0c8bda9ef093e855ce1777019
jarivankaam <jari@jarivankaam.nl>
2026-01-18 20:19:57 +0100

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from './config/config.module';
+import { ConfigModule } from '@nestjs/config';
 import { ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { VkmModule } from './vkm/vkm.module';
 import { SyncModule } from './sync/sync.module';
 import { AuthModule } from './auth/auth.module';
+import configuration from './config/configuration';
+import { ConfigValidationService } from './config/config-validation.service';
 import { QuestionsModule } from './questions/questions.module';
 
 @Module({
   imports: [
-    ConfigModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema: ConfigValidationService.validationSchema,
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: false,
+      },
+    }),
     MongooseModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
